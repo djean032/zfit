6df12bf484418e87c436775a0ebed58ffcde6ad9
djean032 <djean032@gmail.com>
2026-02-06 13:42:30 -0500

    started rewrite into a library

--- src/zfit.f90
+++ src/zfit.f90
@@ -7,97 +7,62 @@ module zfit
   use file_ops
   implicit none
 
-  integer :: idx, zdx, tdx, loc(1), size_exp, i
+contains
+  subroutine fit_zscan_data(x_data, y_data, params, populations, residuals, error, t_slices, z_slices, tau, wavelength, w0, M2,
+  pulse_energy)
+    real(dp), intent(in) :: x_data(:), y_data(:), populations(:), tau, wavelength, w0, M2
+  integer, intent(in) :: t_slices, z_slices
+  real(dp), intent(out) :: params(:), residuals(:), error
+  integer :: z_pos_slices, idx, zdx, tdx, loc(1), size_exp, i
+    real(dp), allocatable :: tot(:), int0(:), z(:), fvec(:), t(:), z_sample(:), intensities(:, :), z_pos(:)
 
   call cpu_time(start)
 
-  ! Get command line arguments
-  estimated_param = spec_pars(3)
+  ! setup important variables
+  wid = tau / (2 * (log(2.0_dp))**0.5)
+  zr = (PI_dp * w0**2) / (M2 * wavelength)
+  frq = c / wavelength
+
+  estimated_param = params(3)
   initial_params = set_par_grid(estimated_param)
 
-  exp = read_exp(filename)
+  z_pos_slices = size(x_data, dim=1)
 
-  z_pos_slices = size(exp, dim=1)
-  allocate (tot(z_pos_slices))
-  allocate (int0(z_pos_slices))
-  allocate (z(z_slices) [*])
-  allocate (fvec(z_pos_slices) [*])
-  allocate (t(t_slices) [*])
-  allocate (z_sample(z_slices) [*])
-  allocate (intensities(z_pos_slices, t_slices) [*])
-  allocate (z_pos(z_pos_slices) [*])
-  allocate (transmission(z_pos_slices) [*])
-  sync all
+  allocate tot(z_pos_slices)
+  allocate int0(z_pos_slices)
+  allocate z(z_slices)
+  allocate fvec(z_pos_slices)
+  allocate t(t_slices)
+  allocate z_sample(z_slices)
+  allocate intensities(z_pos_slices, t_slices)
+  allocate z_pos(z_pos_slices)
 
-  ! Process experimental data into positions and transmissions
+  ! center the data if needed
+  loc = minloc(y_data)
+  z_pos = z_pos - z_pos(loc(1))
 
-  if (this_image() == 1) then
-    z_pos = exp(:, 1) * 1.0e-3_dp
-    transmission = exp(:, 3) / exp(:, 2)
-    transmission = transmission + (1.0_dp - maxval(transmission))
-    loc = minloc(transmission)
-    z_pos = z_pos - z_pos(loc(1))
-    ! Initialize t and z values for DLSODA
-    z = linspace(0.0_dp, 1.0e-1_dp, z_slices)
-    t = linspace(-4.0e-9_dp, 4.0e-9_dp, t_slices)
+  ! Initialize t and z values for DLSODA
+  z = linspace(0.0_dp, 1.0e-1_dp, z_slices)
+  t = linspace(-4.0e-9_dp, 4.0e-9_dp, t_slices)
 
-    do concurrent(zdx=1:z_pos_slices)
-      z_position = z_pos(zdx)
-      do concurrent(tdx=1:t_slices)
-        intensities(zdx, tdx) = irradiance(0.0_dp, t(tdx), z_position)
-      end do
+  do concurrent(zdx=1:z_pos_slices)
+    z_position = z_pos(zdx)
+    do concurrent(tdx=1:t_slices)
+      intensities(zdx, tdx) = irradiance(0.0_dp, t(tdx), z_position)
     end do
-  end if
-  sync all
+  end do
 
-  call co_broadcast(intensities, source_image=1)
-  call co_broadcast(z_pos, source_image=1)
-  call co_broadcast(z_sample, source_image=1)
-  call co_broadcast(transmission, source_image=1)
-  call co_broadcast(z, source_image=1)
-  call co_broadcast(t, source_image=1)
-  call co_broadcast(fvec, source_image=1)
+    call fit_scan(z_pos, transmission, params, z, t, initial_pop, intensities, fvec)
+  error = enorm(z_pos_slices, fvec)
 
 !    initial_pop = [2.90869e17_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp]
 !    spec_pars = [5.48e-18_dp, 16.0e-18_dp, 0.0_dp, 1.0e-12_dp, &
 !                 1.0e-12_dp, 1.0e-12_dp, 1.05e-7_dp,1.0e-12_dp]
 !    initial_params = [3.2e-17_dp, 3.0e-17_dp, 2.6e-17_dp, 3.4e-17_dp, 2.8e-17_dp]
 
-  sync all
-  idx = this_image()
-  params(1) = initial_params(idx)
-  call fit_scan(z_pos, transmission, solve_system, params, z, t, &
-                initial_pop, intensities, fvec)
-  final_params = params(1)
-  rnorm = enorm(z_pos_slices, fvec)
-  sync all
-  call co_min(final_params, result_image=1)
-  call co_min(rnorm, result_image=1)
-
-  if (this_image() == 1) then
-    print *, "Fit Complete"
-    print *, "Best parameters: ", final_params
-    print *, "Best residuals: ", rnorm
-  end if
-  sync all
-
   call cpu_time(finish)
-  call co_max(finish, result_image=1)
-  call co_min(start, result_image=1)
-  if (this_image() == 1) then
-    print *, "Time taken: ", finish - start
-    print *, final_params
-    print *, rnorm
-    tot = solve_system(z_pos, z, t, final_params, initial_pop, intensities)
-    int0 = sum(intensities, dim=2)
-    open (1, file="intensities.dat", status="unknown")
-    open (2, file="exp.dat", status="unknown")
-    do i = 1, z_pos_slices
-      write (1, *) z_pos(i), tot(i)
-      write (2, *) z_pos(i), transmission(i)
-    end do
-  end if
-contains
+  end subroutine
+
   function set_par_grid(par) result(par_grid)
     real(dp), intent(in) :: par
     real(dp) :: par_grid(5)
