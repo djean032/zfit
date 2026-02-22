736aa5f381d8bc91de7e1dd1b1f7c847ddc4a4f8
Dairen Jean <djean032@gmail.com>
2026-02-22 11:58:40 -0500

    made calls threadsafe

--- src/chem.f90
+++ src/chem.f90
@@ -32,6 +32,7 @@ module chem
       subroutine odepack_jac()
       end subroutine
    end interface
+
    interface
       subroutine DLSODA(f, neq, y, t_in, t_out, itol, rtol, atol, itask, &
                              istate, iopt, rwork, lrw, iwork, liw, jac, jt)
@@ -54,7 +55,9 @@ module chem
    public :: rhs_rates, rhs_intensity, jdum, solve_rates, solve_intensity, &
              solve_system, fit_scan
 
-   ! Module-level variables for callback (to avoid nested procedure issue on Linux gfortran)
+   !$omp threadprivate(cb_data_x, cb_data_y, cb_spec_pars, cb_z_samples, &
+   !$omp                cb_times, cb_initial_population, cb_laser_intensities, &
+   !$omp                cb_frq, cb_num_x_pts, cb_num_datasets, cb_expr, cb_y_work)
    real(c_double), pointer :: cb_data_x(:) => null()
    real(c_double), pointer :: cb_data_y(:) => null()
    real(c_double), pointer :: cb_spec_pars(:) => null()
@@ -65,25 +68,24 @@ module chem
    real(c_double) :: cb_frq
    integer(c_int) :: cb_num_x_pts, cb_num_datasets
    procedure(expr_f), pointer :: cb_expr => null()
-   ! Module-level work arrays for ODE solvers (avoid repeated allocation)
-   real(c_double) :: ode_rwork(102)
-   integer(c_int) :: ode_iwork(25)
-   ! Module-level work array for fcn callback
    real(c_double), allocatable :: cb_y_work(:)
-   ! Precomputed constants for rhs_rates (avoid repeated divisions)
+   
+   !$omp threadprivate(pre_inv_hfrq, pre_k1, pre_k2, pre_k3, pre_inv_tau4, pre_inv_tau5, pre_inv_tau6, pre_inv_tau7, pre_inv_tau8)
    real(c_double) :: pre_inv_hfrq, pre_k1, pre_k2, pre_k3, pre_inv_tau4, pre_inv_tau5, pre_inv_tau6, pre_inv_tau7, pre_inv_tau8
 
 contains
+
    subroutine rhs_rates(neq, time, y, ydot)
       integer(c_int), intent(in) :: neq
       real(c_double), intent(in) :: time, y(*)
       real(c_double), intent(out) :: ydot(neq)
       real(c_double) :: i1, i6, r1, r2, r4
+      
       i1 = y(1)
-      i6 = y(6)*pre_inv_hfrq
-      r1 = pre_k1*i1*i6
-      r2 = pre_k2*y(2)*i6
-      r4 = pre_k3*y(4)*i6
+      i6 = y(6) * pre_inv_hfrq
+      r1 = pre_k1 * i1 * i6
+      r2 = pre_k2 * y(2) * i6
+      r4 = pre_k3 * y(4) * i6
       ydot(1) = -r1 + y(2)*pre_inv_tau4 + y(4)*pre_inv_tau7
       ydot(2) = r1 - y(2)*pre_inv_tau4 - r2 + y(3)*pre_inv_tau6 - y(2)*pre_inv_tau5
       ydot(3) = r2 - y(3)*pre_inv_tau6
@@ -95,58 +97,67 @@ contains
       integer(c_int), intent(in) :: neq
       real(c_double), intent(in) :: time, y(*)
       real(c_double), intent(out) :: ydot(neq)
-      ydot(1) = -(pre_k1*y(2) + pre_k2*y(3) + pre_k3*y(5))*y(1)
+      
+      ydot(1) = -(pre_k1*y(2) + pre_k2*y(3) + pre_k3*y(5)) * y(1)
    end subroutine rhs_intensity
 
    subroutine jdum()
    end subroutine jdum
 
-   function solve_rates(y, t_in, t_out) result(y_ret)
-      real(c_double), intent(in) :: t_in, t_out, y(6)
+   subroutine solve_rates(y, t_in, t_out, rwork, iwork, y_ret)
+      real(c_double), intent(in) :: y(6), t_in, t_out
+      real(c_double), intent(inout) :: rwork(102)
+      integer(c_int), intent(inout) :: iwork(25)
+      real(c_double), intent(out) :: y_ret(6)
       integer(c_int) :: istate, neq
-      real(c_double) :: t_ret, y_ret(6)
+      real(c_double) :: t_ret
+      
       neq = 5
       t_ret = t_in
       istate = 1
       y_ret = y
       call DLSODA(rhs_rates, neq, y_ret, t_ret, t_out, 1, &
-                  1e-6_c_double, 1e-6_c_double, 1, istate, 0, ode_rwork, 102, ode_iwork, &
+                  1e-6_c_double, 1e-6_c_double, 1, istate, 0, rwork, 102, iwork, &
                   25, jdum, 2)
-   end function solve_rates
+   end subroutine solve_rates
 
-   function solve_intensity(y, t_in, t_out) result(y_ret)
-      real(c_double), intent(in) :: t_in, t_out, y(6)
+   subroutine solve_intensity(y, t_in, t_out, rwork, iwork, y_ret)
+      real(c_double), intent(in) :: y(6), t_in, t_out
+      real(c_double), intent(inout) :: rwork(102)
+      integer(c_int), intent(inout) :: iwork(25)
+      real(c_double), intent(out) :: y_ret(6)
       integer(c_int) :: istate, neq
-      real(c_double) :: t_ret, y_ret(6)
+      real(c_double) :: t_ret
+      
       neq = 1
       t_ret = t_in
       istate = 1
       y_ret = y
       call DLSODA(rhs_intensity, neq, y_ret, t_ret, t_out, 1, &
-                  1e-6_c_double, 1e-6_c_double, 1, istate, 0, ode_rwork, 102, ode_iwork, &
+                  1e-6_c_double, 1e-6_c_double, 1, istate, 0, rwork, 102, iwork, &
                   25, jdum, 2)
-   end function solve_intensity
+   end subroutine solve_intensity
 
-! Remove globals.
    function solve_system(z_positions, z_samples, times, initial_population, &
                               laser_intensities, frq, spec_pars) result(y)
       !GCC$ attributes dllexport :: solve_system
-    real(c_double), intent(in) :: frq, initial_population(:), laser_intensities(:, :), spec_pars(8), times(:), z_positions(:), z_samples(:)
+      real(c_double), intent(in) :: frq, initial_population(:), laser_intensities(:, :), spec_pars(8), times(:), z_positions(:), z_samples(:)
       integer(c_int) :: pos_idx, sample_idx, t_idx
       real(c_double) :: current_intensity(6), current_population(6), final_intensities(size(z_positions), size(times)), &
                         intensity_0(size(z_positions)), normalized_intensities(size(z_positions)), pop(5, size(z_samples)), &
                         tout, y(size(z_positions)), zout
+      real(c_double) :: rwork(102)
+      integer(c_int) :: iwork(25)
 
-      ! Precompute constants for rhs_rates/rhs_intensity
-      pre_inv_hfrq = 1.0_c_double/(h*frq)
+      pre_inv_hfrq = 1.0_c_double / (h * frq)
       pre_k1 = spec_pars(1)
       pre_k2 = spec_pars(2)
       pre_k3 = spec_pars(3)
-      pre_inv_tau4 = 1.0_c_double/spec_pars(4)
-      pre_inv_tau5 = 1.0_c_double/spec_pars(5)
-      pre_inv_tau6 = 1.0_c_double/spec_pars(6)
-      pre_inv_tau7 = 1.0_c_double/spec_pars(7)
-      pre_inv_tau8 = 1.0_c_double/spec_pars(8)
+      pre_inv_tau4 = 1.0_c_double / spec_pars(4)
+      pre_inv_tau5 = 1.0_c_double / spec_pars(5)
+      pre_inv_tau6 = 1.0_c_double / spec_pars(6)
+      pre_inv_tau7 = 1.0_c_double / spec_pars(7)
+      pre_inv_tau8 = 1.0_c_double / spec_pars(8)
 
       current_intensity(1) = 0.0_c_double
       current_intensity(2:6) = initial_population
@@ -159,10 +170,10 @@ contains
             do sample_idx = 1, size(z_samples)
                current_population(1:5) = pop(:, sample_idx)
                current_population(6) = current_intensity(1)
-               current_population = solve_rates(current_population, 0.0_c_double, tout)
+               call solve_rates(current_population, 0.0_c_double, tout, rwork, iwork, current_population)
                pop(:, sample_idx) = current_population(1:5)
                current_intensity(2:6) = current_population(1:5)
-               current_intensity = solve_intensity(current_intensity, 0.0_c_double, zout)
+               call solve_intensity(current_intensity, 0.0_c_double, zout, rwork, iwork, current_intensity)
             end do
             final_intensities(pos_idx, t_idx) = current_intensity(1)
          end do
@@ -176,7 +187,7 @@ contains
    subroutine fit_scan(data_x, data_y, expr, z_samples, &
                        times, initial_population, laser_intensities, fvec, frq, spec_pars, num_x_pts, num_datasets)
       integer(c_int), intent(in) :: num_datasets, num_x_pts
-    real(c_double), intent(in), target :: data_x(:), data_y(:), initial_population(:), laser_intensities(:, :, :), times(:), z_samples(:)
+      real(c_double), intent(in), target :: data_x(:), data_y(:), initial_population(:), laser_intensities(:, :, :), times(:), z_samples(:)
       real(c_double), intent(inout), target :: spec_pars(8)
       real(c_double), intent(inout) :: fvec(:)
       real(c_double), intent(in) :: frq
@@ -184,7 +195,6 @@ contains
       integer(c_int) :: info, iwa(5)
       real(c_double) :: wa(2*size(fvec)*5 + 5*5 + size(fvec))
 
-      ! Set module-level callback variables
       cb_data_x => data_x
       cb_data_y => data_y
       cb_spec_pars => spec_pars
@@ -200,7 +210,6 @@ contains
 
       call lmdif1(fcn, size(fvec), 1, spec_pars(3), fvec, 1e-3_c_double, info, iwa, wa, size(wa))
 
-      ! Clear pointers
       cb_data_x => null()
       cb_data_y => null()
       cb_spec_pars => null()
@@ -212,7 +221,6 @@ contains
       deallocate(cb_y_work)
    end subroutine fit_scan
 
-   ! Module-level callback for lmdif1
    subroutine fcn(m, n, x, fvec, iflag)
       integer(c_int), intent(in) :: m, n
       integer(c_int), intent(inout) :: iflag
--- src/global.f90
+++ src/global.f90
@@ -1,31 +1,31 @@
 module global
-  use minpack_module
-  use iso_c_binding
-  implicit none
+   use minpack_module
+   use iso_c_binding
+   implicit none
 
-  real(c_double), parameter :: c = 299792458.0_c_double
-  real(c_double), parameter :: h = 6.62607015e-34_c_double
-  real(c_double), parameter :: pi = 4.0_c_double * atan(1.0_c_double)
+   real(c_double), parameter :: c = 299792458.0_c_double
+   real(c_double), parameter :: h = 6.62607015e-34_c_double
+   real(c_double), parameter :: pi = 4.0_c_double*atan(1.0_c_double)
 
 contains
-  function linspace(first, last, n)
-    integer(c_int), intent(in) :: n
-    real(c_double), intent(in) :: first, last
-    integer(c_int) :: i
-    real(c_double) :: linspace(n), span
+   function linspace(first, last, n)
+      integer(c_int), intent(in) :: n
+      real(c_double), intent(in) :: first, last
+      integer(c_int) :: i
+      real(c_double) :: linspace(n), span
 
-    span = last - first
+      span = last - first
 
-    if (n .eq. 0) return
+      if (n .eq. 0) return
 
-    if (n .eq. 1) then
-      linspace(1) = first
-      return
-    end if
+      if (n .eq. 1) then
+         linspace(1) = first
+         return
+      end if
 
-    do i = 1, n
-      linspace(i) = first + span * (i - 1) / (n - 1)
-    end do
-  end function linspace
+      do i = 1, n
+         linspace(i) = first + span*(i - 1)/(n - 1)
+      end do
+   end function linspace
 end module global
 
