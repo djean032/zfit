2edb71902c060f95c074610b5a14bb9dfc3d28c7
Dairen Jean <djean032@gmail.com>
2026-02-23 23:30:16 -0500

    Release v0.1.0

--- src/zfit.f90
+++ src/zfit.f90
@@ -5,16 +5,18 @@ module zfit
    implicit none
 
 contains
-  subroutine fit_zscan_data(x_data, y_data, populations, residuals, error, t_slices, z_slices, sample_width, tau, wavelength, w0, M2, &
-                        pulse_energy, spec_pars, num_x_pts, num_datasets, n_populations, n_residuals) bind(C, name="fit_zscan_data")
-      !GCC$ attributes dllexport :: fit_zscan_data
+   subroutine fit_zscan_data(x_data, y_data, populations, residuals, error, t_slices, z_slices, &
+                             sample_width, tau, wavelength, w0, M2, pulse_energy, spec_pars, &
+                             num_x_pts, num_datasets, n_populations, n_residuals) bind(C, name="fit_zscan_data")
       integer(c_int), intent(in) :: n_populations, n_residuals, num_datasets, num_x_pts, t_slices, z_slices
-      real(c_double), intent(in) :: M2, sample_width, tau, w0, wavelength, populations(*), pulse_energy(*), x_data(*), y_data(*)
+      real(c_double), intent(in) :: M2, sample_width, tau, w0, wavelength, populations(*), &
+                                    pulse_energy(*), x_data(*), y_data(*)
       real(c_double), intent(inout) :: spec_pars(8)
       real(c_double), intent(out) :: error, residuals(*)
       integer(c_int) :: bidx, eidx, idx, loc(1), tdx, zdx
       real(c_double) :: frq, wid, zr
-      real(c_double), allocatable :: data_x(:), data_pop(:), data_y(:), fvec(:), intensities(:, :, :), t(:), z(:)
+      real(c_double), allocatable :: data_x(:), data_pop(:), data_y(:), fvec(:), &
+                                     intensities(:, :, :), t(:), z(:)
 
       allocate (z(z_slices), fvec(num_x_pts*num_datasets))
       allocate (t(z_slices), intensities(num_x_pts, t_slices, num_datasets))
@@ -42,8 +44,9 @@ contains
       do idx = 1, num_datasets
          bidx = (idx - 1)*num_x_pts + 1
          do tdx = 1, t_slices
-             do zdx = 1, num_x_pts
-               intensities(zdx, tdx, idx) = irradiance(0.0_c_double, t(tdx), data_x(bidx + zdx - 1), pulse_energy(idx), wid, w0, zr)
+            do zdx = 1, num_x_pts
+               intensities(zdx, tdx, idx) = irradiance(0.0_c_double, t(tdx), data_x(bidx + zdx - 1), &
+                                                       pulse_energy(idx), wid, w0, zr)
             end do
          end do
       end do
